0e5841353e7d51c9fe64e9e3ecadb2ab3f59c022
Sergio Amo <sergio.amo@outlook.com>
2024-02-11 21:03:11 +0100

    Initial schema

---
+++ app/src/main/java/com/kc/marvel_kc_sp/domain/model/Marvel.kt
@@ -0,0 +1,77 @@
+package com.kc.marvel_kc_sp.domain.model
+
+data class Marvel (
+    val code: Int, // K
+    val status: String, //F
+    val copyright: String, //F
+    val attributionText: String, //F
+    val attributionHTML: String, //F
+    val etag: String, //F
+    val data: Data //K
+)
+
+data class Data (
+    val offset: Int, //FROM
+    val limit: Int,  //TO
+    val total: Int,  //TOTAL
+    val count: Int,  //RETURNED
+    val results: List<MarvelCharacter>
+)
+
+data class MarvelCharacter (
+    val id: Int,
+    val name: String,
+    val description: String,
+    val modified: String,
+    val thumbnail: Thumbnail,
+    val resourceURI: String,
+    val comics: Comics,  // TODO: Decouple this
+    val series: Comics,  // TODO: Decouple this
+    val stories: Stories,
+    val events: Comics, // TODO: Decouple this
+    val urls: List<URL>
+)
+
+data class Comics (
+    val available: Int,
+    val collectionURI: String,
+    val items: List<ComicsItem>,
+    val returned: Int
+)
+
+data class ComicsItem (
+    val resourceURI: String,
+    val name: String
+)
+
+data class Stories (
+    val available: Int,
+    val collectionURI: String,
+    val items: List<StoriesItem>,
+    val returned: Int
+)
+
+data class StoriesItem (
+    val resourceURI: String,
+    val name: String,
+    val type: String
+)
+
+// I'm using a string INSTEAD of an enum for the extension cause as long as android supports it
+// It will work even if the api starts returning webp, avif, heic or other formats...
+data class Thumbnail (
+    val path: String,
+    val extension: String,
+)
+
+data class URL (
+    val type: URLType,
+    val url: String
+)
+
+enum class URLType {
+    Comiclink,
+    Detail,
+    Wiki
+}
+
