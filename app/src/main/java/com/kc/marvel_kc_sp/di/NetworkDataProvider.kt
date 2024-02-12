bb99ae689da56fdddf0b80b59fa4f81dc6a8400b
Sergio Amo <sergio.amo@outlook.com>
2024-02-12 17:54:24 +0100

    Change the way the query parameters are being added as the auth params were overwriting the other parameters.

--- app/src/main/java/com/kc/marvel_kc_sp/di/NetworkDataProvider.kt
+++ app/src/main/java/com/kc/marvel_kc_sp/di/NetworkDataProvider.kt
@@ -1,5 +1,6 @@
 package com.kc.marvel_kc_sp.di
 
+import androidx.room.Query
 import com.kc.marvel_kc_sp.Authentication
 import com.kc.marvel_kc_sp.data.network.NetworkDataSourceImpl
 import com.kc.marvel_kc_sp.data.network.NetworkDataSourceInterface
@@ -11,8 +12,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.QueryMap
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -24,7 +28,10 @@ object NetworkDataProvider {
             val originalRequest = chain.request()
             val queryParams = Authentication.authenticationParams
             val url = originalRequest.url.newBuilder()
-                .encodedQuery(queryParams)
+                //"apikey=$publicKey&ts=$timeStamp&hash=$hash"
+                .addQueryParameter("apikey",queryParams.apikey)
+                .addQueryParameter("ts",queryParams.timeStamp)
+                .addQueryParameter("hash",queryParams.hash)
                 .build()
 
             val newRequest =
