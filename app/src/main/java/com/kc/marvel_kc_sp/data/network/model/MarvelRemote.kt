1b02aafc73f859c6d13fc04af8d784af3c1c8951
Sergio Amo <sergio.amo@outlook.com>
2024-02-14 19:20:57 +0100

    Simplify models

--- app/src/main/java/com/kc/marvel_kc_sp/data/network/model/MarvelRemote.kt
+++ app/src/main/java/com/kc/marvel_kc_sp/data/network/model/MarvelRemote.kt
@@ -6,19 +6,19 @@ import com.squareup.moshi.Json
 // TODO: Decide which data to save, maybe just MarvelCharacterRemote and ignore all the rest?
 data class MarvelRemote (
     @Json(name= "code") val code: Int, // K
-    @Json(name= "status") val status: String, //F
-    @Json(name= "copyright") val copyright: String, //F
-    @Json(name= "attributionText") val attributionText: String, //F
-    @Json(name= "attributionHTML") val attributionHTML: String, //F
-    @Json(name= "etag") val etag: String, //F
+//    @Json(name= "status") val status: String, //F
+//    @Json(name= "copyright") val copyright: String, //F
+//    @Json(name= "attributionText") val attributionText: String, //F
+//    @Json(name= "attributionHTML") val attributionHTML: String, //F
+//    @Json(name= "etag") val etag: String, //F
     @Json(name= "data") val data: DataRemote //K
 )
 
 data class DataRemote (
-    @Json(name= "offset") val offset: Int, //FROM
-    @Json(name= "limit") val limit: Int,  //TO
-    @Json(name= "total") val total: Int,  //TOTAL
-    @Json(name= "count") val count: Int,  //RETURNED
+//    @Json(name= "offset") val offset: Int, //FROM
+//    @Json(name= "limit") val limit: Int,  //TO
+//    @Json(name= "total") val total: Int,  //TOTAL
+//    @Json(name= "count") val count: Int,  //RETURNED
     @Json(name= "results") val results: List<MarvelCharacterRemote>
 )
 
@@ -26,16 +26,16 @@ data class MarvelCharacterRemote (
     @Json(name= "id") val id: Int,
     @Json(name= "name") val name: String,
     @Json(name= "description") val description: String,
-    @Json(name= "modified") val modified: String,
+//    @Json(name= "modified") val modified: String,
     @Json(name= "thumbnail") val thumbnail: ThumbnailRemote,
-    @Json(name= "resourceURI") val resourceURI: String,
-    @Json(name= "comics") val comics: ComicsRemote,  // TODO: Decouple this
-    @Json(name= "series") val series: ComicsRemote,  // TODO: Decouple this
-    @Json(name= "stories") val stories: StoriesRemote,
-    @Json(name= "events") val events: ComicsRemote, // TODO: Decouple this
-    @Json(name= "urls") val urls: List<URLRemote>
+//    @Json(name= "resourceURI") val resourceURI: String,
+//    @Json(name= "comics") val comics: ComicsRemote,  // TODO: Decouple this
+//    @Json(name= "series") val series: ComicsRemote,  // TODO: Decouple this
+//    @Json(name= "stories") val stories: StoriesRemote,
+//    @Json(name= "events") val events: ComicsRemote, // TODO: Decouple this
+//    @Json(name= "urls") val urls: List<URLRemote>
 )
-
+/*
 data class ComicsRemote (
     @Json(name= "available") val available: Int,
     @Json(name= "collectionURI") val collectionURI: String,
@@ -59,7 +59,7 @@ data class StoriesItemRemote (
     @Json(name= "resourceURI") val resourceURI: String,
     @Json(name= "name") val name: String,
     @Json(name= "type") val type: String
-)
+)*/
 
 // I'm using a string INSTEAD of an enum for the extension cause as long as android supports it
 // It will work even if the api starts returning webp, avif, heic or other formats...
@@ -67,8 +67,10 @@ data class ThumbnailRemote (
     @Json(name= "path") val path: String,
     @Json(name= "extension") val extension: String,
 )
+/*
 
 data class URLRemote (
     @Json(name= "type") val type: String,
     @Json(name= "url") val url: String
 )
+*/
--- app/src/main/java/com/kc/marvel_kc_sp/domain/model/Marvel.kt
+++ app/src/main/java/com/kc/marvel_kc_sp/domain/model/Marvel.kt
@@ -2,19 +2,15 @@ package com.kc.marvel_kc_sp.domain.model
 
 data class Marvel (
     val code: Int, // K
-    val status: String, //F
-    val copyright: String, //F
-    val attributionText: String, //F
-    val attributionHTML: String, //F
-    val etag: String, //F
     val data: Data //K
 )
 
 data class Data (
-    val offset: Int, //FROM
+    // Might be useful at some point
+/*    val offset: Int, //FROM
     val limit: Int,  //TO
     val total: Int,  //TOTAL
-    val count: Int,  //RETURNED
+    val count: Int,  //RETURNED*/
     val results: List<MarvelCharacter> // ListOfCharacters
 )
 
@@ -22,39 +18,7 @@ data class MarvelCharacter (
     val id: Int,
     val name: String,
     val description: String,
-    val modified: String,  // TODO: remove?
     val thumbnail: Thumbnail,
-    val resourceURI: String,  // TODO: remove?
-    val comics: Comics,  // TODO: Decouple this
-    val series: Comics,  // TODO: Decouple this
-    val stories: Stories,
-    val events: Comics, // TODO: Decouple this
-    val urls: List<URL> // TODO: remove?
-)
-
-data class Comics (
-    val available: Int,
-    val collectionURI: String,
-    val items: List<ComicsItem>,
-    val returned: Int
-)
-
-data class ComicsItem (
-    val resourceURI: String,
-    val name: String
-)
-
-data class Stories (
-    val available: Int,
-    val collectionURI: String,
-    val items: List<StoriesItem>,
-    val returned: Int
-)
-
-data class StoriesItem (
-    val resourceURI: String,
-    val name: String,
-    val type: String
 )
 
 // I'm using a string INSTEAD of an enum for the extension cause as long as android supports it
@@ -63,16 +27,3 @@ data class Thumbnail (
     val path: String,
     val extension: String,
 )
-
-data class URL (
-    val type: URLType,
-    val url: String
-)
-
-// TODO: consider what to do with this
-enum class URLType {
-    Comiclink,
-    Detail,
-    Wiki
-}
-
