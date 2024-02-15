e026506c1d673a39586ced5daaca8bfb1bf04f6c
Sergio Amo <sergio.amo@outlook.com>
2024-02-15 18:49:19 +0100

    Add an unit test (http to https function)

---
+++ app/src/test/java/com/kc/marvel_kc_sp/utils/UrlProtocolUpgradeKtTest.kt
@@ -0,0 +1,22 @@
+package com.kc.marvel_kc_sp.utils
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.net.URL
+
+class UrlProtocolUpgradeKtTest {
+
+
+    // I'm in a hurry, sorry for not doing more unit tests
+    @Test
+    fun `GIVEN http url THEN https url`() {
+
+        val http = "http://some.url.com"
+        val expected = "https://some.url.com"
+        val actual = urlUpgrade(http)
+
+        assertEquals(expected, actual)
+        assertTrue(URL(actual).protocol == "https")
+    }
+}
