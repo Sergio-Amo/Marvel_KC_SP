9cbc29a90f9c4c11f0628e60f1e679985d481dc2
Sergio Amo <sergio.amo@outlook.com>
2024-02-15 22:56:24 +0100

    Revise TODOs

--- app/src/main/java/com/kc/marvel_kc_sp/MarvelKcApp.kt
+++ app/src/main/java/com/kc/marvel_kc_sp/MarvelKcApp.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 
-// TODO: Improve animations and visuals, add loading screen,
+// TODO: Improve animations and visuals, add loading screen and icon,
 //   use the total value in the logic that controls the infinite scrolls to avoid unnecessary calls
 //   improve reusability and customization of elements
 @HiltAndroidApp
