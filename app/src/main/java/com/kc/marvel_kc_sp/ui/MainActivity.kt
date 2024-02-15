f95a467a1f5e72e8bca3afa40e20011feeb06c8d
Sergio Amo <sergio.amo@outlook.com>
2024-02-15 12:47:23 +0100

    Use NavHost to navigate

--- app/src/main/java/com/kc/marvel_kc_sp/ui/MainActivity.kt
+++ app/src/main/java/com/kc/marvel_kc_sp/ui/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.kc.marvel_kc_sp.ui.list.CharactersListScreen
 import com.kc.marvel_kc_sp.ui.theme.Marvel_KC_SPTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,8 +25,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //NavigationController()
-                    CharactersListScreen()
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "characterslist" ){
+                        composable("characterslist"){
+                            CharactersListScreen()
+                        }
+                    }
                 }
             }
         }
